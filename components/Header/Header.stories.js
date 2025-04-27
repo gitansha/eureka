dd2de7d3216f06a3f414f322c5f85acfc3057db7
Gitansha Aggarwal <43965406+gitansha@users.noreply.github.com>
2025-04-27 03:54:37 +0000

    added header

---
+++ components/Header/Header.stories.js
@@ -0,0 +1,21 @@
+import { Header } from './Header';
+
+export default {
+  title: 'To remove/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const LoggedIn = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut = {};
